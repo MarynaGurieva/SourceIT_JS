e970e3ac7ab8747b8af2c2d00e12e2d9a1d66081
Maryna Gurieva <malexgurjeva@gmail.com>
2016-08-02 12:51:36 +0300

    some corrections to my homework to the lecture from 27 July

--- HomeWork_07.27.js
+++ HomeWork_07.27.js
@@ -42,3 +42,37 @@ console.log(h2.totalMileage);		// 100 km
 /*Реализовать такое поведение, 
 при котором при пробеге одной лошадью определенной дистанции, увеличивается также свойство общего пробега 
 для всех лошадей данного класса.*/
+
+// option #2 - base
+
+Horse.prototype = {
+	constructor: Horse.prototype.constructor,
+	totalMileage: 0
+}
+function Horse(name) {
+	this.name = name;
+
+	var mileage = +prompt("Please, enter the initial mileage of this hourse:", 0);
+	Horse.prototype.totalMileage += mileage;
+
+	Object.defineProperty (this, "mileage", {
+		get: function() {
+			return mileage;
+		}
+	});
+	
+	this.run = function(value) {
+		mileage += value;
+		Horse.prototype.totalMileage += value;
+	};
+};
+
+var h1 = new Horse("the first");		// initial 20 km
+h1.run(30);		// 30 km
+console.log(h1.mileage);		// 50 km
+console.log(h1.totalMileage);		// 50 km
+var h2 = new Horse("the second");		// initial 0 km
+h2.run(70);		// 30 km
+console.log(h2.mileage);		// 70 km
+console.log(h1.totalMileage);		// 120 km
+console.log(h2.totalMileage);		// 120 km
